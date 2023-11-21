2ca9805b57d3e1dc7be1f51aa806922e9b381dba
tamourgh1u <grindmoney19@gmail.com>
2023-11-21 15:01:00 +0100

    connexion

---
+++ hellokant/src/connection/Connexion.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace iutnc\hellokant\query;
+
+class ConnectionFactory
+{
+    protected static $connection;
+
+    public static function makeConnection(array $conf)
+    {
+        $dsn = "mysql:host={$conf['host']};dbname={$conf['database']};charset={$conf['charset']}";
+        $options = [
+            \PDO::ATTR_PERSISTENT => true,
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_EMULATE_PREPARES => false,
+            \PDO::ATTR_STRINGIFY_FETCHES => false,
+        ];
+
+        self::$connection = new \PDO($dsn, $conf['username'], $conf['password'], $options);
+        return self::$connection;
+    }
+
+    public static function getConnection()
+    {
+        return self::$connection;
+    }
+}
