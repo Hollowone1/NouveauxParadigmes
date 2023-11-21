d187b61ce9815d8798587ce912e87f0dafc44a77
tamourgh1u <grindmoney19@gmail.com>
2023-11-21 15:38:20 +0100

    connexion

--- hellokant/src/connection/Connexion.php
+++ hellokant/src/connection/Connexion.php
@@ -8,16 +8,31 @@ class ConnectionFactory
 
     public static function makeConnection(array $conf)
     {
-        $dsn = "mysql:host={$conf['host']};dbname={$conf['database']};charset={$conf['charset']}";
-        $options = [
-            \PDO::ATTR_PERSISTENT => true,
-            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-            \PDO::ATTR_EMULATE_PREPARES => false,
-            \PDO::ATTR_STRINGIFY_FETCHES => false,
-        ];
+        self::$config = $conf;
 
-        self::$connection = new \PDO($dsn, $conf['username'], $conf['password'], $options);
-        return self::$connection;
+        $dbtype = self::$config('db_driver');
+        $host = self::$config('host');
+        $dbname = self::$config('dbname');
+        $user = self::$config('db_user');
+        $pass = self::$config('db_password');
+        $port = ((isset(self::$config['dbport'])) ? self::$config['dbport'] : null);
+        $dsn = "$dbtype:host=$host;dbname=$dbname";
+        try{
+            self::$db = new PDO($dsn,$user,$pass,array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => 
+            PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_STRINGIFY_FETCHES => false,
+            ))
+
+            self::$db->prepare(query:'SET NAMES \'UTF8\'')->execute();
+        }
+            
+        catch(\PDOException $e){
+            throw new DBException(message: "Connection: $dsn", $e->getMessage(). '</br>');
+        }
+        
     }
 
     public static function getConnection()
