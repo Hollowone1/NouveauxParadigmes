9ffd1179cf9f35f8f285e41e871ee9a5055a7b88
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2023-11-21 13:21:35 +0100

    à jour pr TD2 (BDD marche (cf tests ds index))

--- hellokant/public/index.php
+++ hellokant/public/index.php
@@ -1,2 +1,11 @@
 <?php
 echo "bonjour";
+
+
+//tests
+$db = new \PDO('mysql:host=td.article.db;dbname=article', 'article', 'article');
+// use the connection here
+$stmt = $db->prepare('SELECT * FROM article');
+$stmt->execute();
+var_dump($stmt->fetchAll(\PDO::FETCH_ASSOC));
+
