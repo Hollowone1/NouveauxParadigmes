a57e8e97657f69d76714902bd285d0fdfb5db717
Antoine Stoecklin <astoeck@hotmail.fr>
2023-11-21 15:19:37 +0100

    done

---
+++ hellokant/src/connection/Connexion.php
@@ -0,0 +1,18 @@
+<?php
+
+class connexionFactory {
+
+    static $sql;
+
+    static function makeConnexion($conf){
+        
+
+    }
+
+    static function getConnexion(){
+        if(!is_null(static::$sql)) {
+            
+        }
+    }
+
+}
